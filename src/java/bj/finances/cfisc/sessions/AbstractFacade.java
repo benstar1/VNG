f905757690538061377ee0e52c2fdb8eaa9dd516
ben <bentsijohnson@gmail.com>
2016-12-15 14:20:39 +0100

    4EME_COMPIL

--- src/java/bj/finances/cfisc/sessions/AbstractFacade.java
+++ src/java/bj/finances/cfisc/sessions/AbstractFacade.java
@@ -5,8 +5,17 @@
  */
 package bj.finances.cfisc.sessions;
 
+import bj.finances.cfisc.controllers.util.JsfUtil;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
 import javax.persistence.EntityManager;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 
 /**
  *
@@ -22,7 +31,21 @@ public abstract class AbstractFacade<T> {
     protected abstract EntityManager getEntityManager();
 
     public void create(T entity) {
+        //getEntityManager().persist(entity);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+    Set<ConstraintViolation<T>> constraintViolations = validator.validate(entity);
+    if(constraintViolations.size() > 0){
+        Iterator<ConstraintViolation<T>> iterator = constraintViolations.iterator();
+        while(iterator.hasNext()){
+            ConstraintViolation<T> cv = iterator.next();
+            System.err.println(cv.getRootBeanClass().getName()+"."+cv.getPropertyPath() + " " +cv.getMessage());
+
+            JsfUtil.addErrorMessage(cv.getRootBeanClass().getSimpleName()+"."+cv.getPropertyPath() + " " +cv.getMessage());
+        }
+    }else{
         getEntityManager().persist(entity);
+    }
     }
 
     public void edit(T entity) {
