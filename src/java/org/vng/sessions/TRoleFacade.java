0254026970d632350c957eb357177774a3551459
CHARLES <racytof@gmail.com>
2018-10-20 20:54:44 +0100

    MERGE 20102018

--- src/java/org/vng/sessions/TRoleFacade.java
+++ src/java/org/vng/sessions/TRoleFacade.java
@@ -5,9 +5,13 @@
  */
 package org.vng.sessions;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.vng.entities.TRole;
 
 /**
@@ -29,4 +33,45 @@ public class TRoleFacade extends AbstractFacade<TRole> {
         super(TRole.class);
     }
     
+    public  List<TRole>  findListRoleByCategorie(String categorie){
+          Query query;
+          query=getEntityManager().createNamedQuery("TRole.findByRolCat").setParameter("rolCat",categorie);
+          return query.getResultList();
+        
+    }
+    
+    
+     public List<SelectItem> getListeRoleByCategorieDEItem()
+    {
+        List<TRole> list= findListRoleByCategorie("DE");
+        List<SelectItem> item = new ArrayList<>();
+        for (TRole object : list)    
+        { 
+            item.add(new SelectItem(object, ""+object.getRolDesig()));
+        }
+        return item;
+    }
+    
+     
+      public List<SelectItem> getListeRoleByCategorieOPItem()
+    {
+        List<TRole> list= findListRoleByCategorie("OP");
+        List<SelectItem> item = new ArrayList<>();
+        for (TRole object : list)    
+        { 
+            item.add(new SelectItem(object, ""+object.getRolDesig()));
+        }
+        return item;
+    }
+     
+     public List<SelectItem> getListeRoleItem()
+    {
+        List<TRole> list= findAll();
+        List<SelectItem> item = new ArrayList<>();
+        for (TRole object : list)    
+        { 
+            item.add(new SelectItem(object, ""+object.getRolDesig()));
+        }
+        return item;
+    }
 }
